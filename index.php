c712cd43f41651308c9005da8c34cdc58078ed37
just-cmm503 <cmm503-Just>
2017-02-28 16:16:50 +0000

    commit 1

--- index.php
+++ index.php
@@ -4,8 +4,28 @@
 </head>
 <body>
 <p>
-    <?php echo "Hell o Worl d \n "."''Hello . World'' \n"; ?>
+    <?php echo "Hello World "."''Hello . World'' \n"; ?>
     <?php  echo 5*7;?>
 </p>
+<?php
+$Sname1="John";
+$Fname1="Jack";
+
+$Sname2="Mark";
+$Fname2="Alvin";
+
+$Sname3="Colin";
+$Fname3="Berg";
+
+$Sname4="Sarah";
+$Fname4="Callum";
+
+echo "<table>";
+    echo "<tr>".$Sname1."<td>".$Fname1."</td></tr>";
+echo "<tr>".$Sname2."<td>".$Fname2."</td></tr>";
+echo "<tr>".$Sname3."<td>".$Fname3."</td></tr>";
+echo "<tr>".$Sname4."<td>".$Fname4."</td></tr>";
+echo "</table>";
+?>
 </body>
 </html>
