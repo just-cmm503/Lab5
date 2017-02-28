c0b4e36cc7a61050a5cf21c9fa7ec00daed676d7
just-cmm503 <cmm503-Just>
2017-02-28 16:34:02 +0000

    commit 3

--- index.php
+++ index.php
@@ -4,27 +4,28 @@
 </head>
 <body>
 <p>
-    <?php echo "Hello World "."''Hello . World'' \n"; ?>
-    <?php  echo 5*7;?>
+    <?php echo "Hello World " . "''Hello . World'' \n"; ?>
+    <?php echo 5 * 7; ?>
 </p>
 <?php
-$Sname1="John";
-$Fname1="Jack";
+$Sname1 = "John";
+$Fname1 = "Jack";
 
-$Sname2="Mark";
-$Fname2="Alvin";
+$Sname2 = "Mark";
+$Fname2 = "Alvin";
 
-$Sname3="Colin";
-$Fname3="Berg";
+$Sname3 = "Colin";
+$Fname3 = "Berg";
 
-$Sname4="Sarah";
-$Fname4="Callum";
+$Sname4 = "Sarah";
+$Fname4 = "Callum";
 
 echo "<table>";
-    echo "<tr><td>".$Sname1."</td><td>".$Fname1."</td></tr>";
-echo "<tr><td>".$Sname2."</td><td>".$Fname2."</td></tr>";
-echo "<tr><td>".$Sname3."</td><td>".$Fname3."</td></tr>";
-echo "<tr><td>".$Sname4."</td><td>".$Fname4."</td></tr>";
+echo "<tr><td>    Surname    </td><td>    FirstName  </td></tr>";
+echo "<tr><td>" . $Sname1 . "</td><td>" . $Fname1 . "</td></tr>";
+echo "<tr><td>" . $Sname2 . "</td><td>" . $Fname2 . "</td></tr>";
+echo "<tr><td>" . $Sname3 . "</td><td>" . $Fname3 . "</td></tr>";
+echo "<tr><td>" . $Sname4 . "</td><td>" . $Fname4 . "</td></tr>";
 echo "</table>";
 ?>
 </body>
