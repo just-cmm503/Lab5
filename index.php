690582e33a9276b8044754ac6cfc1a3c0ebdb809
just-cmm503 <cmm503-Just>
2017-03-03 17:32:07 +0000

    commit 3

--- index.php
+++ index.php
@@ -28,13 +28,37 @@ echo "<tr><td>" . $Sname3 . "</td><td>" . $Fname3 . "</td></tr> \r\n";
 echo "<tr><td>" . $Sname4 . "</td><td>" . $Fname4 . "</td></tr> \r\n";
 echo "</table>";
 
-if($name1="John"){print "Weldon one <br> \r\n";} else {"Nice try 1 <br> \r\n";}
-if($name1="Mark"){print "Weldon two <br> \r\n";} else {"Nice try 2 <br> \r\n";}
-if($name1="Colin"){print "Weldon three <br> \r\n";} else {"Nice try 3 <br> \r\n";}
+if ($name1 = "John") {
+    print "Weldon one <br> \r\n";
+} else {
+    "Nice try 1 <br> \r\n";
+}
+if ($name1 = "Mark") {
+    print "Weldon two <br> \r\n";
+} else {
+    "Nice try 2 <br> \r\n";
+}
+if ($name1 = "Colin") {
+    print "Weldon three <br> \r\n";
+} else {
+    "Nice try 3 <br> \r\n";
+}
 
-for ($i = 1; $i < 10; $i=$i+1) { print "<p>Hello! $i</p>"; }
+for ($i = 1; $i < 10; $i = $i + 1) {
+    print "<p>Hello! $i</p>";
+}
 
-print "<br> 50\2 <br> HaHa";
+for ($i = 1;
+$i <= 30;
+$i = $i + 1) {
+if (fmod($i, 2) == 0) {
+print "<p>You can get Specs on day $i</p>";
+if (fmod($i, 3) == 0) {
+print "<p>You can get mug on day $i</p>";
+if (fmod($i, 4) == 0) {
+    print "<p>You can get Sussage rolls on day $i</p>";
+
+}
 
 
 ?>
